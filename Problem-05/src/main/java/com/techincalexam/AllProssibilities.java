1971c07b634bc1238cae9f0a49fd4c94e7861e1a
Denver Simonsz <denver@objectoneworld.com>
2021-08-27 15:05:50 +0530

    Update all possibilities method.

--- Problem-05/src/main/java/com/techincalexam/AllProssibilities.java
+++ Problem-05/src/main/java/com/techincalexam/AllProssibilities.java
@@ -1,7 +1,14 @@
 package com.techincalexam;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AllProssibilities {
     public static void main(String[] args) {
 
+    }
+
+    static List<Integer> getAllPossibilites(){
+        return Arrays.asList();
     }
 }
